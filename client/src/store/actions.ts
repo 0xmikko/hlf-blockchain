cecd1d6f44c908c696005f044a150eeb9787f9bd
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-12-13 14:58:35 +0300

    Small fix

--- client/src/store/actions.ts
+++ client/src/store/actions.ts
@@ -6,9 +6,10 @@ import * as network1 from './network1/actions';
 import * as network2 from './network2/actions';
 import * as operations from 'redux-data-connect/lib/operations/actions';
 
-export default {
+const actions = {
   network1,
   network2,
   operations,
 };
 
+export default actions;
